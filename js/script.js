c7a54417a3d797a3aee42426e3c6e2c691a81099
MikaArakawa <mika.arakawa@di-v.co.jp>
2021-06-25 16:36:18 +0900

    0625

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-//ハンバーガーメニュの記述
-
 $('.burger-btn').on('click',function(){ //"burger-btn"をクリックしたら、
   $('.burger-btn').toggleClass('cross'); //ハンバーガーボタンに"cross"というclassをつけたり外したりする
   $('.header-nav').fadeToggle(300); //ヘッダーナビゲーションを300ミリ秒でフェードイン、フェードアウトさせる
   $('body').toggleClass('noscroll') //bodyに対して"noscroll"というclassをつけたり外したりする
   $('.kv').toggleClass('noshow') 
 });
+
+ if( $(window).width() <= 768 ){ //デバイスの幅が768以下のとき
+  $('.nav-item>a').on('click',function(){ //.nav-item>aをクリックすると
+    $('.header-nav').fadeOut(300); //.header-navが0.3秒でフェードアウト(メニューのフェードアウト)
+    $('.burger-btn').removeClass('cross'); // .burger-btnのcrossクラスを削除
+    $('body').removeClass('noscroll'); //bodyのnoscrollクラスを削除
+  });
+}
